0d68d2ff6cdfaac89a61b2c0d310fb0fe11b2f08
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-03-04 10:59:42 -0800

    added LoginFormPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
 
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
