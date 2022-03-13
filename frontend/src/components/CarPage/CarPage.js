94cf8436a3ee74065807acfd6c1bf3e89b2ca2dc
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-03-13 15:01:09 -0700

    added conditional to prevent users from posting reviews for one car more than once

--- frontend/src/components/CarPage/CarPage.js
+++ frontend/src/components/CarPage/CarPage.js
@@ -4,6 +4,7 @@ import { useParams, useHistory } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import '../LandingPage/LandingPage.css'
 import { getOneCar } from "../../store/cars";
+import { getReviewsByCar } from "../../store/reviews";
 import './CarPage.css'
 import CreateReviewModal from "../Reviews/CreateReviewModal";
 import ReviewsPerCar from "../Reviews/ReviewsPerCar";
@@ -23,17 +24,28 @@ function CarPage() {
   const userId = useSelector(state => state.session.user).id
 
   useEffect(() => {
-    dispatch(getOneCar(carId));
+    dispatch(getOneCar(carId)).then(() => dispatch(getReviewsByCar(carId)));
   }, [dispatch]);
 
   if (!car) {
     history.push('/404');
   }
-  //check if logged in user has already posted a review for this car, if so do not show create review modal:
+  //check if logged in user has already posted a review for this car, if so don't show the Post Review button
+  const reviews = useSelector((state) => {
+    return Object.values(state.reviews);
+  });
 
-  //check if logged in user is the owner of this car, if so don't show the Post Review button
+  const userIds = [];
+
+  reviews.forEach(review => {
+    userIds.push(review.userId);
+  });
+
+
+  //check if logged in user is the owner of this car AND  if logged in user
+  //has already posted a review for this car, if so don't show the Post Review button
   let enablePostReviewButton = false;
-  if (car.ownerId !== userId) {
+  if (car.ownerId !== userId && !userIds.includes(userId)) {
     enablePostReviewButton = true;
   }
 
